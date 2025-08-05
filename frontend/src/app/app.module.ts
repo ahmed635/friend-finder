bc46946e999c96a89bca5a3a1ddd2b7168fcb736
Ahmed Khaled <ahmed.khaled.aa123@gmail.com>
2025-08-05 20:41:24 +0300

    chore: update Angular to v15

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -62,7 +62,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, {})
   ],
   providers: [],
   bootstrap: [AppComponent]

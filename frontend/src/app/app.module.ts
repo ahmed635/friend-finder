1a86aa20473006d7db89dc5724b4933586e7af22
Ahmed Khaled <ahmed.khaled.aa123@gmail.com>
2025-08-05 20:27:36 +0300

    chore: update Angular from v10 to v11

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -62,7 +62,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   providers: [],
   bootstrap: [AppComponent]
